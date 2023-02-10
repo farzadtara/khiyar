3ec969867eae1de5bf6a134c95605c7279ab4e33
farzad <farzad.tara@gmail.com>
2023-02-10 14:25:40 +0330

    Clean for start

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="navbar">
+        
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component({name:'Navbar'})
+    export default class Navbar extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+nice
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Hello extends Vue {
+    
+  }
+</script>
