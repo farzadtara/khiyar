cb6914441754a1bd66b9671a7b51fb22f2ad5e33
farzad <farzad.tara@gmail.com>
2023-02-11 10:12:44 +0330

    use css-gg

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="navbar">
-    <div>this is logo </div>
-    <div>this is dark mode </div>
+    <div> <span class="gg-file-document"></span> </div>
+    <div> <span class="gg-sun"></span> </div>
   </div>
 </template>
 
@@ -9,12 +9,21 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({ name: "Navbar" })
-export default class Navbar extends Vue {}
+export default class Navbar extends Vue {
+///css.gg
+
+
+}
 </script>
 
 <style scoped lang="scss">
 #navbar{
     background: rgb(222, 223, 210);
     border-bottom: 1px black solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    
 }
 </style>
